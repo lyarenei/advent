655a6e97f73e8113a00994159fd505f681bcd5ea
Dominik Krivohlavek <github@xkrivo.net>
2024-12-04 20:32:54 +0100

    Task 4, small break

--- 2024/04/04.go
+++ 2024/04/04.go
@@ -2,11 +2,14 @@ package _4
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 func Run(inputFile string) {
-	readFile(inputFile)
+	arr := readFile(inputFile)
+	num := search("XMAS", arr)
+	fmt.Printf("Number of XMAS occurences in crossword: %d\n", num)
 }
 
 func readFile(fileName string) [][]rune {
@@ -31,4 +34,17 @@ func readFile(fileName string) [][]rune {
 	}
 
 	return arr
+}
+
+func search(s string, in [][]rune) int {
+	// 1. - left to right
+	// 2. - right to left
+	// 3. | top to down
+	// 4. | bottom to top
+	// 5. / left bottom to right top
+	// 5. / right top to left bottom
+	// 6. \ left top to right bottom
+	// 7. \ right bottom to left top
+
+	return 0
 }
