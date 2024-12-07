d43d09fb2461f66b3276869e28490371e9f15d1b
Dominik Krivohlavek <github@xkrivo.net>
2024-12-07 19:04:54 +0100

    Task 6, part 1

--- 2024/06/06.go
+++ 2024/06/06.go
@@ -7,10 +7,15 @@ import (
 	"os"
 )
 
+const (
+	PathMarker     = 'X'
+	ObstacleMarker = '#'
+)
+
 func Run(inputFile string) {
 	labMap := readFile(inputFile)
 	predictRoute(labMap)
-	positions := utils.Search2DSimple(labMap, 'x')
+	positions := utils.Search2DSimple(labMap, PathMarker)
 	fmt.Printf("The guard visited %d unique positions before leaving the lab\n", positions)
 }
 
@@ -85,54 +90,84 @@ func predictRoute(arr [][]rune) {
 	}
 }
 
-func guardMove(arr [][]rune, direction Movement, row, col int, guard rune) {
+func guardMove(arr [][]rune, direction Movement, row, col int) {
 	var line []rune
 	var axis utils.Direction
+	var obstacleIdx int
+	var arrRow int
+	var arrCol int
+
 	switch direction {
 	case Up:
 		axis = utils.Vertical
 		// Get line from top edge up to guard
-		line = utils.GetLine(arr, axis, 0, col, row+1)
+		arrRow = 0
+		arrCol = col
+		line = utils.GetLine(arr, axis, arrRow, arrCol, row+1)
 		// Find first obstacle for guard (guard is at the last position in line)
-		obstacleIdx := utils.LastIndex(line, '#')
+		obstacleIdx = utils.LastIndex(line, ObstacleMarker)
 		// Move the guard to the obstacle
-		moveGuard(line, direction, obstacleIdx+1, len(line)-1)
+		moveGuard(line, obstacleIdx+1, len(line)-1)
 		// Update map
-		utils.SetLine2D(arr, axis, 0, col, line)
+		utils.SetLine2D(arr, axis, arrRow, arrCol, line)
 	case Right:
 		axis = utils.Horizontal
 		// Get line from guard up to the right edge
-		line = utils.GetLine(arr, axis, row, col, len(arr[row]))
+		arrRow = row
+		arrCol = col
+		line = utils.GetLine(arr, axis, arrRow, arrCol, len(arr[row]))
 		// Find first obstacle for guard (guard is at the first position in line)
-		obstacleIdx := utils.FirstIndex(line, '#')
+		obstacleIdx = utils.FirstIndex(line, ObstacleMarker)
 		// Move the guard to the obstacle
-		moveGuard(line, direction, 0, obstacleIdx-1)
+		moveGuard(line, 0, obstacleIdx-1)
 		// Update map
-		utils.SetLine2D(arr, axis, row, col, line)
+		utils.SetLine2D(arr, axis, arrRow, arrCol, line)
 	case Down:
 		axis = utils.Vertical
 		// Get line from guard down to the bottom edge
-		line = utils.GetLine(arr, axis, row, col, len(arr))
+		arrRow = row
+		arrCol = col
+		line = utils.GetLine(arr, axis, arrRow, arrCol, len(arr))
 		// Find first obstacle for guard (guard is at the first position in line)
-		obstacleIdx := utils.FirstIndex(line, '#')
+		obstacleIdx = utils.FirstIndex(line, ObstacleMarker)
 		// Move the guard to the obstacle
-		moveGuard(line, direction, 0, obstacleIdx-1)
+		moveGuard(line, 0, obstacleIdx-1)
 		// Update map
 		utils.SetLine2D(arr, axis, row, col, line)
 	case Left:
 		axis = utils.Horizontal
 		// Get line from left edge up to the guard
-		line = utils.GetLine(arr, axis, row, 0, col+1)
+		arrRow = row
+		arrCol = 0
+		line = utils.GetLine(arr, axis, arrRow, arrCol, col+1)
 		// Find first obstacle for guard (guard is at the last position in line)
-		obstacleIdx := utils.LastIndex(line, '#')
+		obstacleIdx = utils.LastIndex(line, ObstacleMarker)
 		// Move the guard (the obstacle is at obstacleIdx)
-		moveGuard(line, direction, obstacleIdx+1, len(line)-1)
+		moveGuard(line, obstacleIdx+1, len(line)-1)
 		// Update map
-		utils.SetLine2D(arr, axis, row, 0, line)
+		utils.SetLine2D(arr, axis, arrRow, arrCol, line)
 	}
+
+	if obstacleIdx == -1 {
+		utils.SetLine2D(arr, axis, arrRow, arrCol, line)
+		return
+	}
+
+	switch direction {
+	case Up:
+		line[obstacleIdx+1] = '>'
+	case Down:
+		line[obstacleIdx-1] = '<'
+	case Left:
+		line[obstacleIdx+1] = '^'
+	case Right:
+		line[obstacleIdx-1] = 'v'
+	}
+
+	utils.SetLine2D(arr, axis, arrRow, arrCol, line)
 }
 
-func moveGuard(path []rune, direction Movement, startIdx int, endIdx int) {
+func moveGuard(path []rune, startIdx int, endIdx int) {
 	sIdx := startIdx
 	if startIdx < 0 {
 		sIdx = 0
@@ -144,21 +179,6 @@ func moveGuard(path []rune, direction Movement, startIdx int, endIdx int) {
 	}
 
 	for i := sIdx; i <= eIdx; i++ {
-		path[i] = 'x'
-	}
-
-	if startIdx < 0 || endIdx < 0 {
-		return
-	}
-
-	switch direction {
-	case Up:
-		path[startIdx] = '>'
-	case Down:
-		path[endIdx] = '<'
-	case Left:
-		path[startIdx] = '^'
-	case Right:
-		path[endIdx] = 'v'
+		path[i] = PathMarker
 	}
 }
